a7ed9b0d3ec0328d419249e7445d65a7ae0a8cd9
Raa <ragil.anomdewantoro@gmail.com>
2015-09-30 03:18:31 -0700

    tugas pertama

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If MyBase.BackColor = Color.White Then
+            MyBase.BackColor = Color.Black
+        Else : MyBase.BackColor = Color.White
+
+        End If
+
+    End Sub
+End Class
